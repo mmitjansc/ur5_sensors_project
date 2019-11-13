064eea733d8ac27fd141aaed1b0853d7077d1047
Marc Mitjans <mmitjans@bu.edu>
2019-11-13 15:56:27 -0500

    Cleaned up Matlab code for 2n order system generation

--- portaudio_ros/second_order_system.m
+++ portaudio_ros/second_order_system.m
@@ -1,13 +1,15 @@
 %% Continuous 2-order system to drive the voice coil
 
+%%%%% User inputs %%%%%%%%%%%%%%%%%%
+st = 2; % Settling time (s)
+f = 100; % Frequency of signal (Hz)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 close all
 format long
 
 syms x y z
 s = tf('s');
-
-st = 2e-0;
-f = 100;
 
 wd = f*2*pi;
 
@@ -25,35 +27,30 @@ k = double(solve(eq == 1));
 
 G = wn^2/(s^2 + 2*e*wn*s + wn^2)*k;
 
-% impulse(G)
-
-% t = 0:0.00001:0.01;
-% yt = k*wn/b.*exp(-e*wn.*t).*sin(wn*b.*t);
-% figure; plot(t,yt)
-
 %% Transform G(s) to H(z)
 
 Ts = 1/44100;
 
 H = c2d(G,Ts,'zoh');
 
-gain = 1;
-H = gain*H;
-
 figure;
 impulse(H)
-yi = impulse(H);
 
 [a,b,c,d] = arg2vars(H.num{1}(2), H.num{1}(3), H.den{1}(2), H.den{1}(3)); 
 
-N = length(yi);
-u = zeros(1,N);
-u(1) = 1;
-y = zeros(1,N);
-
-outs = zeros(1,N);
+a
+b
+c
+d
 
 %% Home-made simulation
+
+% N = length(yi);
+% u = zeros(1,N);
+% u(1) = 1;
+% y = zeros(1,N);
+% 
+% outs = zeros(1,N);
 
 % for i = 1:N
 % 
@@ -66,16 +63,7 @@ outs = zeros(1,N);
 %     y = [0,y(1:end-1)];
 %     
 % end
-
-
+% 
 % figure;
 % plot(0:Ts:(N-1)*Ts,outs)
 % xlabel('Time (s)'); ylabel('Output'); 
-
-yi = impulse(H);
-
-a
-b
-c
-d
-
