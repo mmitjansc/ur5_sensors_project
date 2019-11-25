984320f986f3b0328a1bfde9a4bc0f5cad2aeb62
cair <yychan@bu.edu>
2019-11-25 14:27:07 -0500

    Minor update

--- ur5_joy_control/src/ur5_py_control.py
+++ ur5_joy_control/src/ur5_py_control.py
@@ -9,16 +9,31 @@ import geometry_msgs.msg as gm
 import numpy as np
 from math import pi
 from std_msgs.msg import String
+from sensor_msgs.msg import Joy
 from moveit_commander.conversions import pose_to_list
 
-
-if __name__ == '__main__':
+def joyCallback(data):
 	
+	axes = data.axes
+	buttons = data.buttons
+	
+	print(axes)
+	
+    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+
+
+if __name__ == '__main__':	
 	
 	moveit_commander.roscpp_initialize(sys.argv)
 	rospy.init_node('ur5_node', anonymous=True)
-	rate = rospy.Rate(10) # 10hz
 	
-	pub = rospy.Publisher('chatter_test', String, queue_size=10)
+	rate = rospy.Rate(100) # Hz
+	
+	sub = rospy.Subscriber("joy_subs", Joy, joyCallback)
+	
+	pub = rospy.Publisher("chatter_test", String, queue_size=10)
 
 	print("Entered ROS!")
+	
+	while not rospy.is_shutdown():
+		rate.sleep()
