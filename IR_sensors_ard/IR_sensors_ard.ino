52b7d0b17c4c4e680763d06a85db5239c10d5de5
MarcMitjans <mmitjans@bu.edu>
2019-12-09 14:03:12 -0500

    Added IR publisher to arduino code

--- IR_sensors_ard/IR_sensors_ard.ino
+++ IR_sensors_ard/IR_sensors_ard.ino
@@ -1,10 +1,16 @@
 #include <ros.h>
+#include <std_msgs/Int16.h>
 
 #define THRESH 500
 
 const int analogPin = A0;
 int sensorVal = 0;
 
+// ROS initialization
+ros::NodeHandle nh;
+
+std_msgs::Int16 ir_data;
+ros::Publisher node("/ir_sensor", &ir_data);
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,7 +21,7 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   sensorVal = analogRead(A0);
-  if sensorVal > THRESH {
+  if (sensorVal > THRESH) {
     // Publish something somewhere
   }
   
